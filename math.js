8f3d6b967f9fbce81d236bea1798a32f4c713ccd
vuongcdt <vuongcdt@gmail.com>
2022-03-11 20:29:50 +0700

    them math

--- math.js
+++ math.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function math() {
   return (
-    <div>math</div>
+    <div>math//////////////a+bgit checkout -b feature-3</div>
   )
 }
 console.log('je');
